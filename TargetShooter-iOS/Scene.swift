c15cc78ac2fbc1ccf8195d80838076b04a4ce8ff
mikezander <mikezander87@yahoo.com>
2017-10-13 11:47:05 -0400

    added target touch logic

--- TargetShooter-iOS/Scene.swift
+++ TargetShooter-iOS/Scene.swift
@@ -21,6 +21,10 @@ class Scene: SKScene {
         }
     }
     
+    let startTime = Date()
+    
+    
+    
     override func didMove(to view: SKView) {
         remainingLabel.fontSize = 36
         remainingLabel.fontName = "AmericanTypewriter"
@@ -79,6 +83,23 @@ class Scene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else{ return }
+        
+        let location = touch.location(in: self)
+        let hit = nodes(at: location)
+        
+        if let sprite = hit.first{
+            
+            let scaleOut = SKAction.scale(to: 2, duration: 0.2)
+            let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+            let group = SKAction.group([scaleOut, fadeOut])
+            let sequence = SKAction.sequence([group, SKAction.removeFromParent()])
+            
+            sprite.run(sequence)
+            
+            targetCount -= 1
+        }
         
     }
 }
