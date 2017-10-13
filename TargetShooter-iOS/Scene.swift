c618f4e41021260a7600bc3ea7963d4930d5b419
mikezander <mikezander87@yahoo.com>
2017-10-13 11:56:27 -0400

    added gameOver method

--- TargetShooter-iOS/Scene.swift
+++ TargetShooter-iOS/Scene.swift
@@ -99,7 +99,35 @@ class Scene: SKScene {
             sprite.run(sequence)
             
             targetCount -= 1
+            
+            if targetsCreated == 20 && targetCount == 0{
+                
+                gameOver()
+            }
         }
+    }
+    
+    func gameOver(){
         
+        remainingLabel.removeFromParent()
+        
+        let gameOver = SKSpriteNode(imageNamed: "gameOver")
+        addChild(gameOver)
+        
+        let timeTaken = Date().timeIntervalSince(startTime)
+        let timeLabel = SKLabelNode(text: "Time taken: \(Int(timeTaken)) seconds")
+        
+        timeLabel.fontSize = 36
+        timeLabel.fontName = "AmericanTypeWriter"
+        timeLabel.color = .white
+        timeLabel.position = CGPoint(x: 0, y: -view!.frame.midY + 50)
+        
+        addChild(timeLabel)
+ 
     }
 }
+
+
+
+
+
